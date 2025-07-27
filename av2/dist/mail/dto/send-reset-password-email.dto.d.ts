ed9ada2df5f4e4e9a8aa03ed16b4296e328df251
AbigailPlua08 <e1316055795@live.uleam.edu.ec>
2025-07-26 19:01:13 -0500

    Delete av2 directory

--- av2/dist/mail/dto/send-reset-password-email.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class SendResetPasswordEmailDto {
-    email: string;
-    resetUrl: string;
-}
--- av2/dist/mail/mail.module.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const MAIL_TRANSPORTER = "MAIL_TRANSPORTER";
-export declare class MailModule {
-}
